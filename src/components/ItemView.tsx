5bbef7c82852f3d415bd01ab1c1bac8dcd7369e4
Bikram Ghuku <bikramghuku05@gmail.com>
2024-10-10 23:15:04 +0530

    feat: add styles

--- src/components/ItemView.tsx
+++ src/components/ItemView.tsx
@@ -19,10 +19,12 @@ function ItemView({itemId, onClose, shown}:{itemId:string, onClose: () => void, 
                         <img src={item?.image_url} className='modal-img' />
                     </div>
                     <div className="modal-right">
-                        <div className="modal-name">Name: {item?.name}</div>
-                        <div className="modal-qty">Quantity: {item?.quantity}</div>
-                        <div className="modal-cat">Category: {item?.category}</div>
-                        <div className="modal-brand">Brand: {item?.brand}</div>
+                        <div className="modal-name">Name: {item.name}</div>
+                        <div className="modal-qty">Quantity: {item.quantity}</div>
+                        <div className="modal-cat">Category: {item.category}</div>
+                        <div className="modal-price">Price: ${item.price}</div>
+                        {item.status == "out_of_stock" ? <div className='modal-status-oos'>Out of Stock</div> : <div className='modal-status-is'>In stock</div>}
+                        <div className="modal-brand">Brand: {item.brand}</div>
                         <div className="modal-atri">
                             {atri.map((key, idx) => (
                                 <div key={idx} className="attribute">
