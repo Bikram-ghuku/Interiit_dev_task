d754bb38435c8c0784d0bde8231e57fc58b4d3ce
Bikram Ghuku <bikramghuku05@gmail.com>
2024-10-10 22:32:03 +0530

    feat: added item viewer modal

--- src/components/ItemComp.tsx
+++ src/components/ItemComp.tsx
@@ -1,15 +1,21 @@
 import itemData from '../data/items.json'
 import '../styles/ItemComp.css'
 import obj from '../assets/object.png'
+import ItemView from './ItemView'
+import { useState } from 'react';
 
 function ItemComp({godownId} : {godownId : string}) {
-    const items = itemData.filter((item) => item.godown_id == godownId)
+    const items = itemData.filter((item) => item.godown_id == godownId);
+    const [curr, setCurr] = useState<string>('');
     return (
         <div>
             {items.map((val, idx) => (
-                <div key={idx} className='godown-item'>
-                    <img src={obj} className='godown-item-img'/>
-                    <span className='godown-item-name'>{val.name}</span>
+                <div>
+                    <div key={idx} className='godown-item' onClick={() => setCurr(val.item_id)}>
+                        <img src={obj} className='godown-item-img'/>
+                        <span className='godown-item-name'>{val.name}</span>
+                    </div>
+                    <ItemView itemId={val.item_id} onClose={() => setCurr('')} shown={val.item_id == curr}/>
                 </div>
             ))}
         </div>
---
+++ src/components/ItemView.tsx
@@ -0,0 +1,40 @@
+import itemData from '../data/items.json'
+import '../styles/ItemView.css'
+
+function ItemView({itemId, onClose, shown}:{itemId:string, onClose: () => void, shown:boolean}) {
+    const item = itemData.find((ele) => ele.item_id == itemId);
+    const atri = Object.keys(item!.attributes)
+
+    if (!item) {
+        return <div>Item not found</div>;
+    }
+
+    return (
+        <div className="modal-overlay">
+            <div className="modal-main" hidden={!shown}>
+                <button className="modal-close" onClick={onClose}>X</button>
+                <h2>Item details</h2>
+                <div className="modal-details">
+                    <div className="modal-left">
+                        <img src={item?.image_url} className='modal-img' />
+                    </div>
+                    <div className="modal-right">
+                        <div className="modal-name">Name: {item?.name}</div>
+                        <div className="modal-qty">Quantity: {item?.quantity}</div>
+                        <div className="modal-cat">Category: {item?.category}</div>
+                        <div className="modal-brand">Brand: {item?.brand}</div>
+                        <div className="modal-atri">
+                            {atri.map((key, idx) => (
+                                <div key={idx} className="attribute">
+                                    <strong>{key.replace("_", " ")}: </strong>{String(item.attributes[key as keyof typeof item.attributes])}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ItemView
