76b0ffc3b28ce3c006f5f4af0656140c0032e562
Bikram Ghuku <bikramghuku05@gmail.com>
2024-10-10 18:09:21 +0530

    feat: updated styles and icons

--- src/components/ItemComp.tsx
+++ src/components/ItemComp.tsx
@@ -1,13 +1,16 @@
 import itemData from '../data/items.json'
 import '../styles/ItemComp.css'
-
+import obj from '../assets/object.png'
 
 function ItemComp({godownId} : {godownId : string}) {
     const items = itemData.filter((item) => item.godown_id == godownId)
     return (
         <div>
             {items.map((val, idx) => (
-                <div key={idx} className='godown-item'>{val.name}</div>
+                <div key={idx} className='godown-item'>
+                    <img src={obj} className='godown-item-img'/>
+                    <span className='godown-item-name'>{val.name}</span>
+                </div>
             ))}
         </div>
     )
