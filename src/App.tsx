d2e055f9d7e2678748c76bf113ffca78a7796978
Bikram Ghuku <bikramghuku05@gmail.com>
2024-10-12 20:20:12 +0530

    feat: add browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Godown from './pages/Godown'
 
